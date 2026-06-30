3f291d7797e47af8d660df97404bfd9f287833dc
Korir Winnie Chemutai <chemutaiwinnie43@gmail.com>
2026-06-30 22:16:21 +0300

    Variables make programs dynamic, names used to store values, stored in memory, reusable anytime, Can be updated anytime.

---
+++ 01-basics/variables.py
@@ -0,0 +1,13 @@
+print("My name is Winnie")
+print("Winnie is learning Python")
+print("Winnie wants to become Python Expert") 
+
+name = "Korir"
+print("My name is", name)
+print(name,"is learning Python")
+print(name, "wants to become Python Expert") 
+name = "Chemutai"
+language = "JavaScript"
+print("My name is",name)
+print(name, "is learning",language)
+print(name,"wants to become",language, "expert")
