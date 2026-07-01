6ba967b8b1b366a729d8d22eaab373bfaa6dbed4
Korir Winnie Chemutai <chemutaiwinnie43@gmail.com>
2026-07-01 07:27:22 +0300

    Fixed formatting bug using sep=""

---
+++ 01-basics/python-challenge002.py
@@ -0,0 +1,9 @@
+#Print the following threenlines.
+#Add a variable to make it dynamic.
+#info@datawithkorir.com
+#support@datawithkorir.com
+#www.datawithkorir.com
+name = "datawithkorir"
+print("info@",name,".com\n"
+"support@",name,".com\n"
+"www.",name,".com",sep="")
